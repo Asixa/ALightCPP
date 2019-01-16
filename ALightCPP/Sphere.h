a90b839ce281adff440646be9040c61448d51b48
Asixa <Asixa@foxmail.com>
2019-01-15 20:12:53 -0500

    Weekend Chapter5

--- ALightCPP/Sphere.h
+++ ALightCPP/Sphere.h
@@ -1,13 +1,44 @@
 #pragma once
 #include "vec3.h"
 #include "ray.h"
+#include "Hitable.h"
 
-bool hit_sphere(const vec3& center,float radius,const ray& r)
+class sphere :public hitable
+{
+public:
+	sphere() {}
+	sphere(vec3 cent, float r) :center(cent), radius(r) {};
+	virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec)const;
+	vec3 center;
+	float radius;
+};
+
+inline bool sphere::hit(const ray& r, float tmin, float tmax, hit_record& rec) const
 {
 	vec3 oc = r.origin() - center;
 	float a = dot(r.direction(), r.direction());
-	float b = 2.0*dot(oc, r.direction());
+	float b =dot(oc, r.direction());
 	auto c = dot(oc, oc) - radius * radius;
-	const auto discriminant = b * b - 4 * a*c;
-	return (discriminant > 0);
+	const auto discriminant = b * b - a*c;
+
+	if (discriminant > 0)
+	{
+		float temp = (-b - sqrt(b*b - a * c)) / a;
+		if (temp<tmax&&temp>tmin)
+		{
+			rec.t = temp;
+			rec.p = r.point_at_parameter(rec.t);
+			rec.normal = (rec.p - center) / radius;
+			return true;
+		}
+		temp = (-b + sqrt(b*b - a * c)) / a;
+		if (temp<tmax&&temp>tmin)
+		{
+			rec.t = temp;
+			rec.p = r.point_at_parameter(rec.t);
+			rec.normal = (rec.p - center) / radius;
+			return true;
+		}
+	}
+	return false;
 }
--- ALightCPP/main.cpp
+++ ALightCPP/main.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 #include "GLWindow.h"
 #include "Sphere.h"
+#include "hitable_list.h"
 using namespace std;
 
-
+hitable *world;	hitable *list[2];
 void InitData()
 {
 	PixelLength = ImageHeight*ImageWidth*4;
@@ -14,28 +15,39 @@ void InitData()
 	for (auto i = 0; i < PixelLength; i++)
 		PixelData[i] = static_cast<GLbyte>(int(0));
 }
+void InitScene()
+{
 
+	list[0] = new sphere(vec3(0, 0, -1), 0.5);
+	list[1] = new sphere(vec3(0, -100.5, -1), 100);
+	world = new hitable_list(list, 2);
+
+}
 
 int main(int argc, char* argv[])
 {
 	InitData();
+	InitScene();
 	InitWindow(argc, argv, GLUT_DOUBLE | GLUT_RGBA, 100, 100, ImageWidth, ImageHeight, "ALight");
 	return 0;
 }
 
-vec3 shade(const ray& r)
+vec3 shade(const ray& r,hitable *world)
 {
-	if (hit_sphere(vec3(0, 0, -1), 0.5, r))return vec3(1, 0, 0);
-	vec3 unit_dir = unit_vector(r.direction());
-	float t = 0.5*(unit_dir.y() + 1);
-	return  (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+	hit_record rec;
+	if(world->hit(r,0,99999,rec))
+	{
+		return  0.5*vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+	}
+	else
+	{
+		vec3 unit_dir = unit_vector(r.direction());
+		auto t = 0.5*(unit_dir.y() + 1);
+		return  (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+	}
 }
 void render()
 {
-	// for (auto j = ImageHeight - 1; j >= 0; j--)
-	// 	for (auto i = 0; i < ImageWidth; i++)
-	// 		SetPixel(i, j, &vec3(float(i) / float(ImageWidth), float(j) / float(ImageHeight), 0.2f));
-
 	const vec3 lower_left_corner(-2.0, -2.0, -1.0),horizontal(4, 0, 0), vertical(0, 4, 0), origin(0, 0, 0);
 
 	for (auto j=ImageHeight-1;j>=0;j--)
@@ -43,7 +55,7 @@ void render()
 		{
 			float u = float(i) / float(ImageWidth), v = float(j) / float(ImageHeight);
 			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-			auto c = shade(r);
+			auto c = shade(r,world);
 			SetPixel(i, j, &c);
 		}
 }
